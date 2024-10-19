2b9c63d0f72a824bcb64213c9c146930fbe2a9e3
Enes CAN <enesscann98@gmail.com>
2024-10-19 04:52:29 +0300

    update Task entity's field annotations

--- src/main/kotlin/com/helloworldstudios/todoappbackend/entities/Task.kt
+++ src/main/kotlin/com/helloworldstudios/todoappbackend/entities/Task.kt
@@ -4,18 +4,19 @@ import com.helloworldstudios.todoappbackend.core.entities.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
-import java.time.Instant
 import java.util.*
 
 @Entity
 @Table(name = "tasks")
 data class Task(
-    val title: String,
-    val description: String,
-    @Column(name = "task_status")
-    val taskStatus: Long,
-    @Column(name = "due_date")
-    val dueDate: Date
+    @Column(name = "title", nullable = false)
+    var title: String,
+    @Column(name = "description", nullable = true)
+    var description: String?,
+    @Column(name = "task_status", nullable = false)
+    var taskStatus: Long,
+    @Column(name = "due_date", nullable = true)
+    var dueDate: Date?
 ) : BaseEntity(){
-    constructor() : this("title", "description", -1L, Date.from(Instant.now()))
+    constructor() : this("title", null, -1L, null)
 }
