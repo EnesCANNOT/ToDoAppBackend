b194a41ca4433e122a796bb63cd42ed73bd0cfb0
Enes CAN <enesscann98@gmail.com>
2024-10-19 04:28:38 +0300

    add TaskMapper class

---
+++ src/main/kotlin/com/helloworldstudios/todoappbackend/core/utilities/mapping/TaskMapper.kt
@@ -0,0 +1,20 @@
+package com.helloworldstudios.todoappbackend.core.utilities.mapping
+
+import com.helloworldstudios.todoappbackend.business.dtos.requests.taskRequests.CreateTaskRequest
+import com.helloworldstudios.todoappbackend.entities.Task
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.factory.Mappers
+
+@Mapper
+interface TaskMapper {
+    companion object{
+        val INSTANCE: TaskMapper = Mappers.getMapper(TaskMapper::class.java)
+    }
+
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "task_status", target = "task_status")
+    @Mapping(source = "dueDate", target = "dueDate")
+    fun taskFromCreateTaskRequest(request: CreateTaskRequest): Task
+}
