7eb9a10990f4cca573a223d498d7f0db83c0ac70
Enes CAN <enesscann98@gmail.com>
2024-10-19 04:25:01 +0300

    fixup! add CreateTaskRequest class

--- src/main/kotlin/com/helloworldstudios/todoappbackend/business/dtos/requests/taskRequests/CreateTaskRequest.kt
+++ src/main/kotlin/com/helloworldstudios/todoappbackend/business/dtos/requests/taskRequests/CreateTaskRequest.kt
@@ -1,14 +1,10 @@
 package com.helloworldstudios.todoappbackend.business.dtos.requests.taskRequests
 
-import com.helloworldstudios.todoappbackend.core.entities.GeneralEnumClasses
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
 import java.util.*
 
 data class CreateTaskRequest(
     val title: String,
     val description: String,
-    @Enumerated(EnumType.STRING)
-    val taskStatus: GeneralEnumClasses.TaskStatus,
+    val taskStatus: Long,
     val dueDate: Date
 )
