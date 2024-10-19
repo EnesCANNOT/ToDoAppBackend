a82caccf921ef606a101d01143102e39b6a9b899
Enes CAN <enesscann98@gmail.com>
2024-10-19 03:30:37 +0300

    Bismillah

---
+++ src/test/kotlin/com/helloworldstudios/todoappbackend/ToDoAppBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.helloworldstudios.todoappbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToDoAppBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
