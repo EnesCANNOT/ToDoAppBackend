6ae4964bf7a1dc1bdd445b68004616a9b6c897dd
Enes CAN <enesscann98@gmail.com>
2024-10-19 04:35:27 +0300

    update CreatedTaskResponse

--- src/main/kotlin/com/helloworldstudios/todoappbackend/business/dtos/responses/taskResponses/CreatedTaskResponse.kt
+++ src/main/kotlin/com/helloworldstudios/todoappbackend/business/dtos/responses/taskResponses/CreatedTaskResponse.kt
@@ -1,15 +1,11 @@
 package com.helloworldstudios.todoappbackend.business.dtos.responses.taskResponses
 
-import com.helloworldstudios.todoappbackend.core.entities.GeneralEnumClasses
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
 import java.util.*
 
 data class CreatedTaskResponse(
     val id: Long,
     val title: String,
     val description: String,
-    @Enumerated(EnumType.STRING)
-    val taskStatus: GeneralEnumClasses.TaskStatus,
+    val taskStatus: Long,
     val dueDate: Date,
 )
