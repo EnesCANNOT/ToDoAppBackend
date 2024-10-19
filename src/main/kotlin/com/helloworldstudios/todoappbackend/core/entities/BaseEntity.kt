932dfb567eee19b7b2d184af54cd8d0800687ab5
Enes CAN <enesscann98@gmail.com>
2024-10-19 03:32:56 +0300

    add BaseEntity

---
+++ src/main/kotlin/com/helloworldstudios/todoappbackend/core/entities/BaseEntity.kt
@@ -0,0 +1,40 @@
+package com.helloworldstudios.todoappbackend.core.entities
+
+import jakarta.persistence.*
+import lombok.AllArgsConstructor
+import lombok.Data
+import lombok.NoArgsConstructor
+import java.time.Instant
+import java.util.*
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+open class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long? = null
+
+    @Column(name = "createdDate")
+    var createdDate: Date? = null
+
+    @Column(name = "updatedDate")
+    var updatedDate: Date? = null
+
+    @Column(name = "deletedDate")
+    val deletedDate: Date? = null
+
+    @PrePersist
+    fun onCreate() {
+        createdDate = Date.from(Instant.now())
+        println(createdDate)
+    }
+
+    @PreUpdate
+    fun onUpdate() {
+        updatedDate = Date.from(Instant.now())
+        println(updatedDate)
+    }
+}
